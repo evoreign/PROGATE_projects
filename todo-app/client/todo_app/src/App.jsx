957459593c05fd5d45f48fdb5cf3e21839941b27
evoreign <edbert_khovey@yahoo.com>
2023-08-04 18:06:15 +0700

    add warning in app.jsx if server is not running

--- todo-app/client/todo_app/src/App.jsx
+++ todo-app/client/todo_app/src/App.jsx
@@ -1,13 +1,38 @@
-
-import './App.css'
-import TodoContainer from './components/todoContainer'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import TodoContainer from './components/todoContainer';
 
 function App() {
+  const [isBackendAvailable, setIsBackendAvailable] = useState(true);
+
+  // Function to check backend availability
+  const checkBackendAvailability = async () => {
+    try {
+      await fetch('http://localhost:1337/api/todos');
+    } catch (error) {
+      setIsBackendAvailable(false);
+    }
+  };
+
+  useEffect(() => {
+    checkBackendAvailability();
+  }, []);
+
   return (
     <div>
-      <TodoContainer></TodoContainer>
+      {isBackendAvailable ? (
+        <TodoContainer />
+      ) : (
+        <div>
+          <h2 style={{ color: 'red' }}>
+            Backend is currently unreachable.
+          </h2>
+          <TodoContainer />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
