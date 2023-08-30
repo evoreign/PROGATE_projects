4532e2d529b29a8be911e9e60b55e88d2bcb3dc1
evoreign <edbert_khovey@yahoo.com>
2023-08-31 03:26:07 +0700

    mounting clerkprovider

--- react native/corp-wellness/App.tsx
+++ react native/corp-wellness/App.tsx
@@ -1,21 +1,25 @@
-
+import { ClerkProvider } from '@clerk/clerk-expo'
 import { StyleSheet, Text, View, SafeAreaView, StatusBar} from 'react-native'
 import Header from './src/components/header'
 import Progress from './src/components/progress'
 import Schedule from './src/components/schedule'
 import Nav from './src/components/nav'
 
+const publishableKey = 'pk_test_cmVsZXZhbnQtamF5YmlyZC04OS5jbGVyay5hY2NvdW50cy5kZXYk'
 export default function App() {
 	return (
-    <SafeAreaView style={{flex:1, marginTop: StatusBar.currentHeight}}>
-      <View style={styles.container}>
-        <Header />
-        <Progress />
-        <Schedule />
-        <Nav/>
-        {/* modal here incoming */}
-      </View>
-    </SafeAreaView>
+    <ClerkProvider publishableKey={publishableKey}>
+      <SafeAreaView style={{flex:1, marginTop: StatusBar.currentHeight}}>
+        <View style={styles.container}>
+          <Header />
+          <Progress />
+          <Schedule />
+          <Nav/>
+          {/* modal here incoming */}
+        </View>
+      </SafeAreaView>
+    </ClerkProvider>
+    
 		
 	)
 }
