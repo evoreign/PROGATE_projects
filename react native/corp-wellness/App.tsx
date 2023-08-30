73960ffae7dd89f520ece34040c5405673d590ff
evoreign <edbert_khovey@yahoo.com>
2023-08-31 02:26:35 +0700

    redo the corp-wellness react native app because stuff happened some weird bug with clerk hopefully by redoing it it will fix it.

--- react native/corp-wellness/App.tsx
+++ react native/corp-wellness/App.tsx
@@ -1,9 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Schedule() {
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text>schedule</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
