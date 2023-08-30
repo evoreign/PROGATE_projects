d295f0bd37eaa782ee81c1d657e3871bb3d87fd5
evoreign <edbert_khovey@yahoo.com>
2023-08-31 03:39:06 +0700

    add token caching to app

--- react native/corp-wellness/App.tsx
+++ react native/corp-wellness/App.tsx
@@ -1,14 +1,39 @@
+// Library import 
 import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo'
 import { StyleSheet, Text, View, SafeAreaView, StatusBar} from 'react-native'
+import * as SecureStore from "expo-secure-store";
+
+//  Component import
 import Header from './src/components/header'
 import Progress from './src/components/progress'
 import Schedule from './src/components/schedule'
 import Nav from './src/components/nav'
 import SignInWithOAuth from './src/components/signin/SignInWithOAuth'
+
+// Clerk key yeah its public because somehow env variable is not working
 const publishableKey = 'pk_test_cmVsZXZhbnQtamF5YmlyZC04OS5jbGVyay5hY2NvdW50cy5kZXYk'
+
+// SecureStore for token caching
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
 export default function App() {
 	return (
-    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <SafeAreaView style={{flex:1, marginTop: StatusBar.currentHeight}}>
         
         {/* This is for the signed in user */}
