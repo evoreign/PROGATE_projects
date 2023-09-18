7f45cf502bd384bb28f0b81751d897b04d7d8235
edbert khovey <64061171+evoreign@users.noreply.github.com>
2023-09-18 18:55:55 +0700

    Expo EAS config to build

--- react-native/corp-wellness/app.config.js
+++ react-native/corp-wellness/app.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-    name: 'MyApp',
-    version: '1.0.0',
-    extra: {
-      clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    
+    expo: {
+      name: 'MyApp',
+      version: '1.0.0',
+      extra: {
+        
+        "eas": {
+          "projectId": "3b7d271d-f720-4b20-8b87-5138535fbd8b"
+        }
+      },
+      "updates": {
+        "url": "https://u.expo.dev/3b7d271d-f720-4b20-8b87-5138535fbd8b"
+      },
+      "runtimeVersion": {
+        "policy": "appVersion"
+      },
+      "android": {
+        "package": "com.example.MyApp",
+        "versionCode": 1
+      }
     },
 };
